0a4a32fdde3dd57fc867aa7c086f14497b4d6588
Nayem <nayem.alam@mail.mcgill.ca>
2018-08-14 20:46:50 -0400

    UI

---
+++ README 2.md
@@ -0,0 +1,5 @@
+# McGillGD-Group2
+Ubisoft Game Lab Competition 2018
+
+afjdkskfndsnfklsmckmlxz
+afjdkskfndsnfklsmckmlxzcasx
---
+++ User Interface/User Interface/Assets/score.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class score : MonoBehaviour {
+
+	public Transform player;
+	public Text scoreText;
+	// Update is called once per frame
+	void Update () {
+		scoreText.text = player.position.x.ToString("0");
+		scoreText.text = player.position.y.ToString("0");
+
+
+		
+	}
+}
